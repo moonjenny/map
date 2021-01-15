110f95b5747f76e1dafa240b80f0e97394abfa0b
moon yh <danny18521@naver.com>
2021-01-16 00:07:40 +0900

    mapp html

---
+++ mobile/js/common.ui.js
@@ -0,0 +1,57 @@
+window.onload = function(){
+	//main
+
+	//nav-menu
+	var sidebarBtn = $(".btn-close-sidebar");
+	var sidebarBox = $(".map-sidebar");
+
+	sidebarBtn.click(function(){
+		$(this).toggleClass('sidebar-close');
+		sidebarBox.toggleClass('sidebar-close');
+	});
+
+	//location
+	var btnLocation = $(".top-location li > a");
+	btnLocation.click(function(e){
+		e.preventDefault();
+		$(this).parent().find(".sub-location").toggleClass("open");
+	});
+
+	//input file
+	$(".upload-file input[type='file']").change(function(){
+	  //alert("파일 올라감");
+		var fileName = $(this).val().replace(/C:\\fakepath\\/i, '');
+		$(this).parent().parent().find("p").text(fileName);
+	});
+
+	//map-feed-list
+	$(document).ready(function(){
+		$(window).resize(throttle(100, function(e) {
+			resizeContents();
+		}));
+		resizeContents();
+	});
+		
+	function resizeContents() {
+		var feedWrite = $(".feed-box-fixed").outerHeight() + 10;
+		$(".map-feed-list").css({
+			paddingBottom : feedWrite + 'px'
+		});
+	}
+	// map-feed-list 주어진 시간만큼 지나야 주어진 함수를 실행 합니다.
+	function throttle(ms, fn) {
+		var allow = true;
+		function enable() {
+			allow = true;
+		}
+		return function(e) {
+			if(allow) {
+				allow = false;
+				setTimeout(enable, ms);
+				fn.call(this, e);
+			}
+		}
+	}
+
+
+};
---
+++ pc/js/common.ui.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	//main
+
+	//sidevar scrollbar
+	$(".map-feed-list").mCustomScrollbar({
+		theme:"dark-thick"
+	});
+
+	//popup-reply scrollbar
+	$(".reply-content").mCustomScrollbar({
+		theme:"dark-thick"
+	});
+
+	//nav-menu
+	var sidebarBtn = $(".btn-close-sidebar");
+	var sidebarBox = $(".map-sidebar");
+
+	sidebarBtn.click(function(){
+		$(this).toggleClass('sidebar-close');
+		sidebarBox.toggleClass('sidebar-close');
+	});
+
+	//location
+	var btnLocation = $(".top-location li > a");
+	btnLocation.click(function(e){
+		e.preventDefault();
+		$(this).parent().find(".sub-location").toggleClass("open");
+	});
+
+	//input file
+	$(".upload-file input[type='file']").change(function(){
+	  //alert("파일 올라감");
+		var fileName = $(this).val().replace(/C:\\fakepath\\/i, '');
+		$(this).parent().parent().find("p").text(fileName);
+	});
+
+
+});
